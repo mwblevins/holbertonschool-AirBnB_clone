f956b4311ef79fef60f80afde0d068f9f4d54fde
Philip <philip.dan.taylor@gmail.com>
2023-02-02 19:34:14 +0000

    Add file storeage and start creating tests

---
+++ models/engine/file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""File Storeage class"""
+import json
+
+
+class FileStorage():
+
+    __file_path = ""
+    __objects = {}
+
+    def all(self):
+        """retrieve __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Add Object to __objects"""
+        key_str = "{}.{}".format(obj.__class__.__name__, obj.id or "ERROR")
+        FileStorage.__objects[key_str] = obj
+
+    def save(self):
+        """Save __objects to __file_path"""
+        with open(FileStorage.__file_path, "w") as objFile:
+            json.dump(objFile, objFile)
+
+    def reload(self):
+        """Load objects from __file_path"""
+        with open(FileStorage.__file_path, "r") as fileobj:
+            FileStorage.__objects = json.load(fileobj)
