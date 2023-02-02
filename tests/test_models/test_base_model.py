4107d4a57eace7923d5847cde464d5cd0ab24783
Philip <philip.dan.taylor@gmail.com>
2023-02-02 18:25:23 +0000

    Update Git Ignore, Add empty Tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Base Class Test Cases"""
+import unittest
+from models.base import Base
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_contructors(self):
+        pass
+    
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
