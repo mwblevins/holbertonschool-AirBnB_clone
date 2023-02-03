40edbeaa0d0e5131d0f3aeceedcaf99175a33e1b
mwblevins <michaelwblevins@gmail.com>
2023-02-03 19:59:43 +0000

    Up to date until 'update'

--- console.py
+++ console.py
@@ -12,6 +12,8 @@ class HBNBCommand(cmd.Cmd):
     supported_classes = ["BaseModel", "User", "State",
                          "City", "Amenity", "Place", "Review"]
 
+    blackList = ["id", "created_at", "updated_at"]
+    
     def do_quit(self, line):
         """Quit command to exit the program."""
         return True
@@ -66,7 +68,7 @@ class HBNBCommand(cmd.Cmd):
             return  
         class_name, instance_id = args[0], args[1]
         if class_name not in HBNBCommand.supported_classes:
-            print("** class dosent exist **")
+            print("** class doesnt exist **")
             return
 
         key = "{}.{}".format(class_name, instance_id)
@@ -75,6 +77,42 @@ class HBNBCommand(cmd.Cmd):
         storage.remove(key)
         storage.save()
         return
+    
+    def do_all(self, line):
+        """Prints all string represenation of all instances based or not on the class name"""
+        if line == "":
+            print([str(i) for i in storage.all().values()])
+            return
+        
+        if line in HBNBCommand.supported_classes:
+            print([str(i) for k, i in storage.all().items() if line in k])
+        else:
+            print("** class doesnt exist **")
+        
+    def do_update(self, line):
+        """Udates an instance based on the class name and id"""
+        errMsgs = ["class name missing", "instance id missing",
+                   "attribute name missing", "value missing"]
+        args = line.split()
+        if len(args) < 4:
+            print("** {} **".format(errMsgs[len(args)]))
+            return
+        if args[0] not in HBNBCommand.supported_classes:
+            print("** class doesnt exist **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        target = storage.all().get(key)
+        if target is None:
+            print("** no instance found **")
+            return
+        if args[2] in HBNBCommand.blackList:
+            return
+        setattr(target, args[2], eval(args[3]))
+            
+        
+             
+        
+        
     
         
             
