a18ab84247228b081d6403c664fda4a55886d0d0
mwblevins <michaelwblevins@gmail.com>
2023-02-03 19:06:18 +0000

    console through step: 'delete'

--- console.py
+++ console.py
@@ -3,10 +3,14 @@
 
 
 import cmd
-
+from models.base_model import *
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    
+    supported_classes = ["BaseModel", "User", "State",
+                         "City", "Amenity", "Place", "Review"]
 
     def do_quit(self, line):
         """Quit command to exit the program."""
@@ -19,6 +23,66 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on an empty line."""
         pass
+    
+    def do_create(self, line):
+        """Creates a neew instance of BaseModel, saves and prints id"""
+        if not line:
+            print("** class name missing **")
+        else:
+            try:
+                cls = eval(line)()
+                cls.save()
+                print(cls.id)
+            except (NameError, AttributeError):
+                print('** class doesnt exist **')
+
+    def do_show(self, line):
+        """prints the string represenation of an instance based on the class name/id"""
+        if not line:
+            print("** class name missing **")
+            return
+
+        args = line.split()
+        if len(args) < 2:
+            print("** instance id missing **")
+            return  
+        class_name, instance_id = args[0], args[1]
+        if class_name not in HBNBCommand.supported_classes:
+            print("** class dosent exist **")
+            return
+
+        key = "{}.{}".format(class_name, instance_id)
+        res = storage.all().get(key) or ("** no instance found **")
+        print(res)
+
+    def do_destroy(self, line):
+        """Deletes and instance based on the class name and id"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        if len(args) < 2:
+            print("** instance id missing **")
+            return  
+        class_name, instance_id = args[0], args[1]
+        if class_name not in HBNBCommand.supported_classes:
+            print("** class dosent exist **")
+            return
+
+        key = "{}.{}".format(class_name, instance_id)
+        if key not in storage.all().keys():
+            print("** no instance found **")
+        storage.remove(key)
+        storage.save()
+        return
+    
+        
+            
+        
+            
+                
+        
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
