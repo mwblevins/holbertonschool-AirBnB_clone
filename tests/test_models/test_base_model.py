9922119bb64a5b7241f106c6e8d13b065d0a5d19
Philip <philip.dan.taylor@gmail.com>
2023-02-05 17:54:09 +0000

    Adds a few more tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,28 +6,30 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """TASK 1 UNIT TESTS"""
-    def test_bas_mod_id(self):
+    def test_base_model_id(self):
+        """Test that id is assigned as a string"""
         bm1 = BaseModel()
         self.assertIsInstance(bm1.id, str)
 
-    def test_bas_mod_crt(self):
+    def test_base_model_create(self):
+        """Test that created_at is stored as datetime"""
         bm1 = BaseModel()
         self.assertIsInstance(bm1.created_at, datetime)
 
-    def test_bas_mod_upd(self):
+    def test_base_model_update(self):
         bm1 = BaseModel()
         self.assertIsInstance(bm1.updated_at, datetime)
 
-    def test_uwu_id(self):
+    def test_unique_id(self):
         bm1 = BaseModel()
         bm2 = BaseModel()
         self.assertNotEqual(bm1.id, bm2.id)
 
-    def test_ini_tim(self):
+    def test_init_time(self):
         bm1 = BaseModel()
         self.assertEqual(bm1.created_at, bm1.updated_at)
 
-    def test_sav_upd_met(self):
+    def test_save_update(self):
         bm1 = BaseModel()
         created = bm1.created_at
         updated = bm1.updated_at
@@ -42,6 +44,32 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(dict_model["updated_at"], str)
         self.assertIsInstance(dict_model["created_at"], str)
 
-    def test_str_met(self):
+    def test_str_(self):
         bm1 = BaseModel()
         self.assertIn(bm1.id, str(bm1))
+
+
+    def test_init_with_kwargs(self):
+        created_at = '2023-04-20T00:00:00.000000'
+        updated_at = '2023-04-20T00:00:00.000000'
+        template = {
+            'id': 'villager',
+            'created_at': created_at,
+            'updated_at': updated_at,
+            'name': 'Robert'
+        }
+        bm1 = BaseModel(**template)
+        self.assertEqual(bm1.id, 'villager')
+        self.assertEqual(bm1.created_at, datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%f'))
+        self.assertEqual(bm1.updated_at, datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%S.%f'))
+        self.assertEqual(bm1.name, 'Robert')
+
+    def test_to_dict_and_back(self):
+        """Test for full path of recreating from dictionary """
+        bm1 = BaseModel()
+        bm1_str = str(bm1)
+        bm2_template = bm1.to_dict()
+        bm2 = BaseModel(**bm2_template)
+        self.assertEqual(str(bm2),bm1_str)                  # Test for Equality, as this should print all atributes
+        self.assertIsInstance(bm2.created_at, datetime)     # Follow up with other tests ensuring
+        self.assertIsInstance(bm2.updated_at, datetime)     # the object was recreated properly.
