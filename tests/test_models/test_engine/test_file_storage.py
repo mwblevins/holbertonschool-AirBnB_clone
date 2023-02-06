afd3d8b858998a8e2d552ccda8550565e9bef085
Philip <philip.dan.taylor@gmail.com>
2023-02-06 18:15:16 +0000

    add More Tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base Class Test Cases"""
 import unittest
+import os
 import json
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
@@ -8,61 +9,63 @@ from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
 
-    testObject = None
+    file_storage_tester = None
+    NeedCleanup = False
 
     def test_contructors(self):
-        self.testObject.reload()
-        start_count = len(self.testObject.all())
+        self.file_storage_tester.reload()
+        start_count = len(self.file_storage_tester.all())
         obj = BaseModel()
-        self.testObject.new(obj)
-        self.assertEqual(len(self.testObject.all()), start_count + 1)
+        self.file_storage_tester.new(obj)
+        self.assertEqual(len(self.file_storage_tester.all()), start_count + 1)
         pass
 
-    def setUp(self) -> None:
-        self.testObject = FileStorage()
-        return super().setUp()
-
     def tearDown(self) -> None:
+        if self.NeedCleanup:
+            if os.path.exists("file.json"):
+                os.remove("file.json")
+            os.rename("temp.json","file.json")
         return super().tearDown()
 
     def setUp(self):
         """initialize objects and file storage instance"""
-        self.obj1 = {"id": 1, "name": "Test1"}
-        self.obj2 = {"id": 2, "name": "Test2"}
-        self.fs = FileStorage()
+        if os.path.exists("file.json"):
+            os.rename("file.json","temp.json")
+            self.NeedCleanup = True
+        self.obj1 = BaseModel(**{"id": 1, "name": "Test1"})
+        self.obj2 = BaseModel(**{"id": 2, "name": "Test2"})
+        self.file_storage_tester = FileStorage()
+        
 
     def test_all(self):
         """Test if all method retrieves objects correctly"""
-        self.fs.new(self.obj1)
-        self.fs.new(self.obj2)
-        self.assertDictEqual(self.fs.all(), {"dict.1": self.obj1, "dict.2": self.obj2})
+        start = len(self.file_storage_tester.all())
+        self.file_storage_tester.new(self.obj1)
+        self.file_storage_tester.new(self.obj2)
+        self.assertEqual(len(self.file_storage_tester.all()), start + 2)
 
     def test_new(self):
         """Test if new method adds object to __objects correctly"""
-        self.fs.new(self.obj1)
-        self.assertDictEqual(self.fs.all(), {"dict.1": self.obj1})
+        self.file_storage_tester.new(self.obj1)
+        self.assertIn(self.obj1,self.file_storage_tester.all().values())
 
-    def test_remove(self):
-        """Test if remove method deletes the correct object from __objects"""
-        self.fs.new(self.obj1)
-        self.fs.new(self.obj2)
-        self.fs.remove("dict.1")
-        self.assertDictEqual(self.fs.all(), {"dict.2": self.obj2})
-
-    def test_save(self):
-        """Test if save method saves objects to file correctly"""
-        self.fs.new(self.obj1)
-        self.fs.new(self.obj2)
-        self.fs.save()
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as fileobj:
-            data = json.load(fileobj)
-            self.assertDictEqual(data, {"dict.1": self.obj1, "dict.2": self.obj2})
+    def test_remove_method(self):
+        """Tests if the remove method deletes an object from the dict"""
+        self.file_storage_tester.remove("BaseModel.{}".format(self.obj1.id))
+        all_objects = self.file_storage_tester.all()
+        self.assertNotIn("BaseModel.{}".format(self.obj1.id), all_objects)
 
     def test_reload(self):
         """Test if reload method loads objects from file correctly"""
-        self.fs.new(self.obj1)
-        self.fs.new(self.obj2)
-        self.fs.save()
-        self.fs.__objects = {}
-        self.fs.reload()
-        self.assertDictEqual(self.fs.all(), {"dict.1": self.obj1, "dict.2": self.obj2})
+        self.file_storage_tester.new(self.obj1)
+        self.file_storage_tester.new(self.obj2)
+        preSaveAll = self.file_storage_tester.all()
+        self.file_storage_tester.save()
+        self.file_storage_tester.__objects = {}
+        self.file_storage_tester.reload()
+        self.assertDictEqual(self.file_storage_tester.all(), preSaveAll)
+
+    def test_save_method(self):
+        """Tests if the save method creates a file with the objects data"""
+        self.file_storage_tester.save()
+        self.assertTrue(os.path.exists("file.json"))
