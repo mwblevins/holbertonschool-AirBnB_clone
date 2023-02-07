1d5d2fa0ed4df866a6cd3eb83ea295738d106de7
Philip <philip.dan.taylor@gmail.com>
2023-02-07 19:22:28 +0000

    Correct Name of user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,18 +18,19 @@ class TestUser(unittest.TestCase):
         obj1.first_name = "Johnjacob"
         obj1.last_name = "Jinglehimmersmith"
         self.assertNotEqual(obj2.first_name, obj1.first_name)
+        self.assertNotEqual(obj2.last_name, obj1.last_name)
 
     def test_init(self):
         self.assertEqual(User, type(User()))
 
-    def test_email_pub(self):
+    def test_email_type(self):
         self.assertEqual(str, type(User.email))
 
-    def test_paswd_pub(self):
+    def test_password_type(self):
         self.assertEqual(str, type(User.password))
 
-    def test_fname_pub(self):
+    def test_first_name_type(self):
         self.assertEqual(str, type(User.first_name))
 
-    def test_lname_pub(self):
+    def test_last_name_type(self):
         self.assertEqual(str, type(User.last_name))
