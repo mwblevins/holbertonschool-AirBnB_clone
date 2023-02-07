acec147b2295e678135039dfba4972aa9a082071
Philip <philip.dan.taylor@gmail.com>
2023-02-07 19:09:23 +0000

    Adds User Tests

--- tests/test_models/tests_user.py
+++ tests/test_models/tests_user.py
@@ -15,5 +15,21 @@ class TestUser(unittest.TestCase):
         self.assertIsNotNone(obj1.email)
         self.assertIsNotNone(obj1.password)
         obj2 = User()
-        obj1.first_name = "Jacob"
+        obj1.first_name = "Johnjacob"
+        obj1.last_name = "Jinglehimmersmith"
         self.assertNotEqual(obj2.first_name, obj1.first_name)
+
+    def test_init(self):
+        self.assertEqual(User, type(User()))
+
+    def test_email_pub(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_paswd_pub(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_fname_pub(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_lname_pub(self):
+        self.assertEqual(str, type(User.last_name))
